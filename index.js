0fa843179ad6a021a19d5c5864e3fb202e89cde6
Iqra <iqra.ali@student.moringaschool.com>
2023-04-12 08:31:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -23,20 +23,7 @@ function houseList(houses){
     </div>
     </article>
     `
-    div.addEventListener("click", ()=>{
-        fetch("http://localhost:5000/house", {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                image:`${houses.image}`,
-            type_house:`${houses.type_house}`,
-            price:`${houses.price}`
-            })
-        }).then(r => r.json())
-        .then(data => console.log(data))
-    })
+              
     dispHouse.appendChild(div)
 }
 const form = document.querySelector(".form").addEventListener("submit", (e)=> {
@@ -69,61 +56,4 @@ const form = document.querySelector(".form").addEventListener("submit", (e)=> {
     }).then(r => r.json())
     .then(data => console.log(data))
   });
-
-//   const searchInput = document.getElementById('search-bar');
-// const searchResults = document.getElementById('search-results');
-// const houseDetails = document.getElementById('house-details');
-
-// searchInput.addEventListener('input', function() {
-//   const searchQuery = this.value;
-
-//   fetch(`houses.json`)
-//     .then(response => response.json())
-//     .then(data => {
-//       let results = '';
-
-//       data.forEach(house => {
-//         if (house.type.toLowerCase().includes(searchQuery.toLowerCase())) {
-//           results += `<div class="house" data-name="${house.name}" data-type="${house.type}" data-location="${house.location}" data-images="${house.images}" data-price="${house.price}">
-//                         <h2>${house.name}</h2>
-//                         <p>Type: ${house.type}</p>
-//                         <p>Location: ${house.location}</p>
-//                       </div>`;
-//         }
-//       });
-
-//       searchResults.innerHTML = results;
-//       houseDetails.innerHTML = '';
-//     });
-// });
-
-// searchResults.addEventListener('click', function(event) {
-//   const selectedHouse = event.target.closest('.house');
-
-//   if (selectedHouse) {
-//     const name = selectedHouse.getAttribute('data-name');
-//     const type = selectedHouse.getAttribute('data-type');
-//     const location = selectedHouse.getAttribute('data-location');
-//     const images = selectedHouse.getAttribute('data-images').split(',');
-//     const price = selectedHouse.getAttribute('data-price');
-
-//     let imageHTML = '';
-//     images.forEach(image => {
-//       imageHTML += `<img src="${image}" alt="${name}" class="house-image">`;
-//     });
-
-//     houseDetails.innerHTML = `<div>
-//                                  ${imageHTML}
-//                                </div>
-//                                <div>
-//                                  <p>Name: ${name}</p>
-//                                  <p>Type: ${type}</p>
-//                                  <p>Location: ${location}</p>
-//                                  <p>Price: ${price}</p>
-//                                </div>`;
-//   }
-// });
-
-
-
 
